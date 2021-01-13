0637c493484b3b3e48d825808f5ea0d063e5f7e3
Emma Cline <emma@cline.engineer>
2021-01-12 21:30:15 -0700

    Add InMemoryDatabase.get stub with psuedocode

--- lib/InMemoryDatabase.ts
+++ lib/InMemoryDatabase.ts
@@ -6,4 +6,11 @@ interface InMemoryDatabaseInstance<K, V> {
 }
 
 export default class InMemoryDatabase<K, V> implements Database<K, V> {
+
+	get(key: K): V | null {
+		// if currentTransaction exists get the value from the transaction first. if the value in the transaction is null be sure to return null since that means the key will be removed from the database instance if present.
+		// if no transaction get the value from the databaseInstance.
+		// if no value exists in either the transaction or the databaseInstance return null
+		return null
+	}
 }
